85fab1902cdfa377b667c09a70ca6afef661394a
JThiagarajah <joshuathiagarajah@gmail>
2026-05-01 09:15:42 +0530

    Added Registration routes

---
+++ routes/registrationRoutes.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { createRegistration, getRegistrations } from "../controllers/registrationController.js";
+
+const router = express.Router();
+
+router.post("/", createRegistration);
+router.get("/", getRegistrations);
+
+export default router;
